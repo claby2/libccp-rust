f0409a757f2ce77db198001632c72a23f5aa7360
Akshay Narayan <akshayn@mit.edu>
2019-02-04 11:09:11 -0500

    build: Modify build.rs to make cargo package happy

--- build.rs
+++ build.rs
@@ -5,12 +5,34 @@ use std::path::PathBuf;
 
 fn main() {
     let mut libccp_make = std::process::Command::new("make")
+        .arg("libccp.a")
         .current_dir("./libccp")
         .spawn()
         .expect("libccp make failed");
     libccp_make.wait().expect("libccp make spawned but failed");
 
-    println!("cargo:rustc-link-search=./libccp");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    std::process::Command::new("mv")
+        .arg("./libccp/libccp.a")
+        .arg(out_path.join("libccp.a"))
+        .spawn()
+        .expect("mv static library failed")
+        .wait()
+        .expect("mv spawned but failed");
+
+    std::process::Command::new("make")
+        .arg("clean")
+        .current_dir("./libccp")
+        .spawn()
+        .expect("libccp make clean failed")
+        .wait()
+        .expect("libccp make clean spawned but failed");
+
+    println!(
+        "cargo:rustc-link-search={}",
+        out_path.to_str().expect("OUT_DIR error")
+    );
     println!("cargo:rustc-link-lib=static=ccp");
 
     let bindings = bindgen::Builder::default()
@@ -21,7 +43,6 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("libccp.rs"))
         .expect("Unable to write bindings");
