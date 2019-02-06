6e975b5d3f42a7f7afa1828300c39015ae5ad83f
Akshay Narayan <akshayn@mit.edu>
2019-02-06 12:17:55 -0500

    datapath: implement Deref(Mut) + Drop
    Remove manual function calls through Rust's trait system

--- src/lib.rs
+++ src/lib.rs
@@ -43,9 +43,40 @@ pub trait DatapathOps {
 
 struct DatapathObj(Box<DatapathOps>);
 
+/// Represents datapath functionality.
+/// libccp state is freed when this is dropped.
 pub struct Datapath(i8);
 
 impl Datapath {
+    /// Initialize libccp and pass it an implementation of `Datapath` functionality.
+    pub fn init<T: DatapathOps + 'static>(dp: T) -> Result<Self, failure::Error> {
+        // need 2 levels of Box so we can avoid passing a fat pointer down
+        let dp = Box::new(DatapathObj(Box::new(dp)));
+        let mut dp = ccp::ccp_datapath {
+            set_cwnd: Some(ccp::set_cwnd),
+            set_rate_abs: Some(ccp::set_rate_abs),
+            set_rate_rel: Some(ccp::set_rate_rel),
+            time_zero: time::precise_time_ns(),
+            now: Some(ccp::now),
+            since_usecs: Some(ccp::since_usecs),
+            after_usecs: Some(ccp::after_usecs),
+            send_msg: Some(ccp::send_msg),
+            impl_: Box::into_raw(dp) as *mut std::os::raw::c_void,
+        };
+
+        let ok = unsafe { ccp::ccp_init(&mut dp) };
+        match ok {
+            i if i >= 0 => (),    // ok
+            -1 => unreachable!(), // ccp_init checks that we didn't pass null function pointers in with `dp`, but we didn't
+            -2 => bail!("Cannot initialize libccp twice"),
+            -3 | -4 | -5 => bail!("Could not alloc"),
+            i if i < -5 => unreachable!(), // ccp_init only returns error codes -1 | -2 | -3 | -4 | -5
+            _ => unreachable!(), // because rust can't figure out that (i < -5 | -5 | -4 | -3 | -2 | -1 | i >= 0) is exhaustive
+        };
+
+        Ok(Datapath(0))
+    }
+
     /// When the datapath receives an IPC message from the congestion
     /// control algorithm, call this function to tell libccp about it.
     pub fn recv_msg(&mut self, msg: &mut [u8]) -> Result<(), failure::Error> {
@@ -65,39 +96,10 @@ impl Datapath {
     }
 }
 
-/// Initialize libccp and pass it an implementation of `Datapath` functionality.
-pub fn init_with_datapath<T: DatapathOps + 'static>(dp: T) -> Result<Datapath, failure::Error> {
-    // need 2 levels of Box so we can avoid passing a fat pointer down
-    let dp = Box::new(DatapathObj(Box::new(dp)));
-    let mut dp = ccp::ccp_datapath {
-        set_cwnd: Some(ccp::set_cwnd),
-        set_rate_abs: Some(ccp::set_rate_abs),
-        set_rate_rel: Some(ccp::set_rate_rel),
-        time_zero: time::precise_time_ns(),
-        now: Some(ccp::now),
-        since_usecs: Some(ccp::since_usecs),
-        after_usecs: Some(ccp::after_usecs),
-        send_msg: Some(ccp::send_msg),
-        impl_: Box::into_raw(dp) as *mut std::os::raw::c_void,
-    };
-
-    let ok = unsafe { ccp::ccp_init(&mut dp) };
-    match ok {
-        i if i >= 0 => (),    // ok
-        -1 => unreachable!(), // ccp_init checks that we didn't pass null function pointers in with `dp`, but we didn't
-        -2 => bail!("Cannot initialize libccp twice"),
-        -3 | -4 | -5 => bail!("Could not alloc"),
-        i if i < -5 => unreachable!(), // ccp_init only returns error codes -1 | -2 | -3 | -4 | -5
-        _ => unreachable!(), // because rust can't figure out that (i < -5 | -5 | -4 | -3 | -2 | -1 | i >= 0) is exhaustive
-    };
-
-    Ok(Datapath(0))
-}
-
-/// Call this function only if you will no longer use libccp.
-/// It will de-allocate libccp's internal state.
-pub fn deinit(_: Datapath) {
-    unsafe { ccp::ccp_free() }
+impl Drop for Datapath {
+    fn drop(&mut self) {
+        unsafe { ccp::ccp_free() }
+    }
 }
 
 /// Implement this trait on the type that holds
@@ -124,6 +126,10 @@ pub trait CongestionOps {
 }
 
 impl CongestionOps {
+    fn downcast<T: CongestionOps>(&self) -> &T {
+        unsafe { &*(self as *const Self as *const T) }
+    }
+
     fn downcast_mut<T: CongestionOps>(&mut self) -> &mut T {
         unsafe { &mut *(self as *mut Self as *mut T) }
     }
@@ -192,13 +198,6 @@ impl<T: CongestionOps + 'static> Connection<T> {
         ))
     }
 
-    /// Get a mutable reference to the T: impl CongestionOps
-    /// that was passed in at the beginning.
-    pub fn conn_state(&mut self) -> &mut T {
-        let y = &mut *(self.1);
-        y.0.downcast_mut::<T>()
-    }
-
     /// Inform libccp of new measurements.
     pub fn load_primitives(&mut self, prims: Primitives) {
         unsafe {
@@ -224,13 +223,29 @@ impl<T: CongestionOps + 'static> Connection<T> {
 
         Ok(())
     }
+}
 
-    /// Call this function when a connection ends.
-    pub fn end(self) {
+impl<T: CongestionOps> Drop for Connection<T> {
+    fn drop(&mut self) {
         unsafe {
             let index = (*(self.0)).index;
             ccp::ccp_connection_free(index);
         }
+    }
+}
+
+impl<T: CongestionOps> std::ops::Deref for Connection<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        let y = &*(self.1);
+        y.0.downcast::<T>()
+    }
+}
+
+impl<T: CongestionOps> std::ops::DerefMut for Connection<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        let y = &mut *(self.1);
+        y.0.downcast_mut::<T>()
     }
 }
 
@@ -275,11 +290,7 @@ mod tests {
 
     fn make_dp(expected_msgs: Vec<Option<Vec<u8>>>) -> Datapath {
         let dp = Dp { expected_msgs };
-        init_with_datapath(dp).unwrap()
-    }
-
-    fn free_dp(d: Datapath) {
-        deinit(d)
+        Datapath::init(dp).unwrap()
     }
 
     fn make_conn(d: &Datapath) -> Connection<Cn> {
@@ -293,8 +304,8 @@ mod tests {
             .with_mss(10)
             .with_four_tuple(1, 2, 3, 4);
 
-        let mut c = Connection::start(d, cn, fi).unwrap();
-        assert_eq!(c.conn_state().curr_cwnd, 19);
+        let c = Connection::start(d, cn, fi).unwrap();
+        assert_eq!(c.curr_cwnd, 19);
         c
     }
 
@@ -362,9 +373,5 @@ mod tests {
         dp.recv_msg(&mut install_basic).unwrap();
         dp.recv_msg(&mut changeprog_msg).unwrap();
         c.invoke().unwrap();
-
-        c.end();
-        free_dp(dp);
-        return;
     }
 }
